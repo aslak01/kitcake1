3f17de800dc11df371fb57269bc1baf025522cf3
aslak bakkeland <aslak.bakkeland@everyday.no>
2021-05-14 16:44:21 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pancake in Svelte Kit
+
+Looking into implementing Rich Harris' Pancake library in Svelte Kit.
---
+++ svelte.config.js
@@ -0,0 +1,11 @@
+import node from '@sveltejs/adapter-node'
+
+const config = {
+	kit: {
+		adapter: node(),
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+	}
+};
+
+export default config;
