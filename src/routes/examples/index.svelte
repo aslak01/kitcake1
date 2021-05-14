2724f95f6c0443e709da7e92001e5fa6bc046ce1
aslak bakkeland <aslak.bakkeland@everyday.no>
2021-05-15 00:36:28 +0200

    More examples and changes to examples index

--- src/routes/examples/index.svelte
+++ src/routes/examples/index.svelte
@@ -1,13 +1,18 @@
 <script context="module">
-	const modules = import.meta.glob('./*/index.svelte')
+	// const modules = import.meta.glob('./*/index.svelte')
 </script>
 <script>
 	import LifeExpt from './lifeexp/index.svelte';
 	import Co2 from './co2chart/index.svelte'
 	import Pop from './population/index.svelte'
 
-	const modules = import.meta.glob('./*/index.svelte')
-	$: console.log("import.meta.glob", modules)
+	import StackedBars from './stackedBars/index.svelte'
+	import StackedColumns from './stackedColumns/index.svelte'
+	import StackedArea from './stackedArea/index.svelte'
+
+
+	// const modules = import.meta.glob('./*/index.svelte')
+	// $: console.log("import.meta.glob", modules)
 
 </script>
 
@@ -23,6 +28,9 @@
 			{/each}
 		{/if}
 	</pre> -->
+	<StackedArea />
+	<StackedColumns />
+	<StackedBars />
 	<Co2 />
 	<Pop />
 	<LifeExpt />
