65c9902ef8501b2e440449afaf930933dae96392
aslak <aslak.bakkeland@everyday.no>
2022-02-26 15:03:32 +0100

    chore: updated config to new syntax + cleanup comments

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,9 @@
-//import node from '@sveltejs/adapter-node'
-import vercel from '@sveltejs/adapter-vercel';
+import vercel from '@sveltejs/adapter-vercel'
 
 const config = {
   kit: {
-    adapter: vercel(),
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte'
-    // vite: {
-    //   ssr: {
-    //     noExternal: []
-    //   },
-    //   optimizeDeps: {
-    //     include: ['@sveltejs/pancake']
-    //   }
-    // }
+    adapter: vercel()
   }
-};
+}
 
-export default config;
+export default config
